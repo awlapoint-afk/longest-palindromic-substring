69a9a8732735c01116da068ff418b200835b7e44
adam <awlapoint@gmail.com>
2025-08-08 23:22:23 -0400

    Initial working version. Is slow, uses string slicing

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def is_palindrome(s1):
+            if s1 == s1[::-1]:
+                return True
+            else:
+                return False
+
+        result_dict = {}
+
+        s_len = len(s)
+        for left in range(s_len):
+            right = s_len - 1
+            while right >= left:
+                if is_palindrome(s[left:right+1]):
+                    result_dict[len(s[left:right+1])] = s[left:right+1]
+                right -= 1
+
+        return result_dict[max(result_dict)]
+
+
+solution = Solution()
+print(solution.longestPalindrome("babad"))
+print(solution.longestPalindrome("cbbd"))
+print(solution.longestPalindrome("a"))
+print(solution.longestPalindrome("ab"))
+print(solution.longestPalindrome("reifadyqgztixemwswtccodfnchcovrmiooffbbijkecuvlvukecutasfxqcqygltrogrdxlrslbnzktlanycgtniprjlospzhhgdrqcwlukbpsrumxguskubokxcmswjnssbkutdhppsdckuckcbwbxpmcmdicfjxaanoxndlfpqwneytatcbyjmimyawevmgirunvmdvxwdjbiqszwhfhjmrpexfwrbzkipxfowcbqjckaotmmgkrbjvhihgwuszdrdiijkgjoljjdubcbowvxslctleblfmdzmvdkqdxtiylabrwaccikkpnpsgcotxoggdydqnuogmxttcycjorzrtwtcchxrbbknfmxnonbhgbjjypqhbftceduxgrnaswtbytrhuiqnxkivevhprcvhggugrmmxolvfzwadlnzdwbtqbaveoongezoymdrhywxcxvggsewsxckucmncbrljskgsgtehortuvbtrsfisyewchxlmxqccoplhlzwutoqoctgfnrzhqctxaqacmirrqdwsbdpqttmyrmxxawgtjzqjgffqwlxqxwxrkgtzqkgdulbxmfcvxcwoswystiyittdjaqvaijwscqobqlhskhvoktksvmguzfankdigqlegrxxqpoitdtykfltohnzrcgmlnhddcfmawiriiiblwrttveedkxzzagdzpwvriuctvtrvdpqzcdnrkgcnpwjlraaaaskgguxzljktqvzzmruqqslutiipladbcxdwxhmvevsjrdkhdpxcyjkidkoznuagshnvccnkyeflpyjzlcbmhbytxnfzcrnmkyknbmtzwtaceajmnuyjblmdlbjdjxctvqcoqkbaszvrqvjgzdqpvmucerumskjrwhywjkwgligkectzboqbanrsvynxscpxqxtqhthdytfvhzjdcxgckvgfbldsfzxqdozxicrwqyprgnadfxsionkzzegmeynye"))
